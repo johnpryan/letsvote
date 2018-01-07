7f83af1aa2c87632b91f6d87249caa1cb6a8854d
John Ryan <john.p.ryan4@gmail.com>
2018-01-07 12:11:22 -0800

    add letsvote shared library

---
+++ letsvote/example/letsvote_example.dart
@@ -0,0 +1,6 @@
+import 'package:letsvote/letsvote.dart';
+
+main() {
+  var awesome = new Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ letsvote/test/letsvote_test.dart
@@ -0,0 +1,16 @@
+import 'package:letsvote/letsvote.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    Awesome awesome;
+
+    setUp(() {
+      awesome = new Awesome();
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
